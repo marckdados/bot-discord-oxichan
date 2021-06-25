0f3394fb571d583cbee93ca036fb654daf5b60b9
Bismarck Nóbrega <62849385+marckdados@users.noreply.github.com>
2021-06-25 12:38:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+
+const Discord = require("discord.js"); 
+const client = new Discord.Client(); 
+const config = require("./config.json"); 
+
+client.login(process.env.TOKEN);
